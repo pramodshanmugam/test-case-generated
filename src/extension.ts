e9c9230f0f364768f0d228814c38ffc86ac2e299
pramod-shanmugam <pramod.sn@webknot.in>
2024-01-20 15:27:09 -0500

    Serach code created but not working

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test-case-generated" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-case-generated.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test Case Generated!');
+	let disposable = vscode.commands.registerCommand('test-case-generated.helloWorld', async () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const code = editor.document.getText();
+			try {
+				const apiResponse = await sendCodeToApi(code);
+				insertApiResponse(editor, apiResponse);
+			} catch (error) {
+				vscode.window.showErrorMessage('Error processing your request');
+				console.error(error);
+			}
+		} else {
+			vscode.window.showInformationMessage('Open a file to use this command');
+		}
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+// Function to send code to an API
+async function sendCodeToApi(code: string): Promise<string> {
+	try {
+		// Replace with your API endpoint and adjust as necessary
+		const response = await axios.post('http://127.0.0.1:8000/endpoint/api/print-data/', { code: code });
+		return response.data; // Adjust this based on how your API responds
+	} catch (error) {
+		console.error('Error sending code to API:', error);
+		throw error;
+	}
+}
+
+// Function to insert API response into the editor
+function insertApiResponse(editor: vscode.TextEditor, apiResponse: string) {
+	editor.edit(editBuilder => {
+		editBuilder.insert(editor.selection.start, apiResponse);
+	});
 }
 
 // This method is called when your extension is deactivated
