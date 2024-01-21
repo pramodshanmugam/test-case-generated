3b4565696c33b78a54bb1cd36bad5e85fd21d76c
pramod-shanmugam <pramod.sn@webknot.in>
2024-01-21 13:09:16 -0500

    Changed functionality to check only views.py and log openai response

--- src/extension.ts
+++ src/extension.ts
@@ -3,34 +3,32 @@ import axios from 'axios';
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "test-case-generated" is now active!');
+    console.log('Congratulations, your extension "test-case-generated" is now active!');
 
-	let disposable = vscode.commands.registerCommand('test-case-generated.helloWorld', async () => {
-		const editor = vscode.window.activeTextEditor;
+    let disposable = vscode.workspace.onDidSaveTextDocument(async (document) => {
+		console.log("saved")
+        if (document.fileName.endsWith('views.py')) {
+            const code = document.getText();
+            try {
+                const apiResponse = await sendCodeToApi(code);
+                console.log(apiResponse); // Log the response
+            } catch (error) {
+                vscode.window.showErrorMessage('Error processing your request');
+                console.error(error);
+            }
+        }
+    });
 
-		if (editor) {
-			const code = editor.document.getText();
-			try {
-				const apiResponse = await sendCodeToApi(code);
-				console.log(apiResponse); // Log the response
-			} catch (error) {
-				vscode.window.showErrorMessage('Error processing your request');
-				console.error(error);
-			}
-		} else {
-			vscode.window.showInformationMessage('Open a file to use this command');
-		}
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
+
 
 // Function to send code to an API
 async function sendCodeToApi(code: string): Promise<any> {
 	try {
 		const data = {
 			model: "gpt-3.5-turbo",
-			messages: [{ "role": "user", "content": code }]
+			messages: [{ "role": "user", "content": code + "Write Unit Test Cases for this using pytest"}]
 		};
 
 		const config = {
